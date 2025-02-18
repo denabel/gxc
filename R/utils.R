33bf078d4e65dab0976e6cdb60c0727c27df2024
Abel <Dennis.Abel@gesis.org>
2025-02-18 19:36:34 +0100

    update utils

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,14 @@
 # R/utils.R
 
+# Allowed catalogues
+allowed_catalogues_monthly <- c(
+  "reanalysis-era5-land-monthly-means",
+  "reanalysis-era5-single-levels-monthly-means"
+)
 
+allowed_catalogues_daily <- c(
+  "derived-era5-land-daily-statistics",
+  "derived-era5-single-levels-daily-statistics"
+)
 
 
