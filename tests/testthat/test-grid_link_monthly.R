45774fe234db5a4435fb23b39e7cad6e385614d0
Abel <Dennis.Abel@gesis.org>
2025-03-09 22:44:04 +0100

    test set up

--- tests/testthat/test-grid_link_monthly.R
+++ tests/testthat/test-grid_link_monthly.R
@@ -1,8 +1,8 @@
-test_that("grid_link_daily returns a SpatRaster with appended focal_value when baseline is FALSE", {
+test_that("grid_link_monthly returns a SpatRaster with appended focal_value when baseline is FALSE", {
   skip_on_cran()
 
   if (Sys.getenv("WF_API_KEY") == "") {
-    skip("API key not set, skipping grid_link_daily test")
+    skip("API key not set, skipping grid_link_monthly test")
   }
 
   api_key <- Sys.getenv("WF_API_KEY")
@@ -12,18 +12,17 @@ test_that("grid_link_daily returns a SpatRaster with appended focal_value when b
 
   sample_grid <- rast(ncols = 10, nrows = 10, xmin = 0, xmax = 1, ymin = 0, ymax = 1, crs = "EPSG:4326")
 
-  terra::time(sample_grid) <- as.Date("2014-08-01")
+  terra::time(sample_grid) <- seq(as.Date("2014-01-01"), by = "1 month", length.out = nlyr(sample_grid))
 
-  result <- grid_link_daily(
+  result <- grid_link_monthly(
     indicator = "2m_temperature",
     data = sample_grid,
     time_span = 0,
     time_lag = 0,
-    baseline = c("1970","1971"),
+    baseline = FALSE,
     path = tempdir(),
-    catalogue = "derived-era5-land-daily-statistics",
-    statistic = "daily_mean",
-    time_zone = "utc+00:00",
+    catalogue = "reanalysis-era5-land-monthly-means",
+    by_hour = FALSE,
     method = "bilinear",
     keep_raw = FALSE,
     parallel = FALSE,
--- tests/testthat/test-point_link_monthly.R
+++ tests/testthat/test-point_link_monthly.R
@@ -1,34 +1,32 @@
-test_that("point_link_daily returns an sf object when baseline is TRUE", {
+test_that("point_link_monthly returns an sf object with appended focal_value when baseline is FALSE", {
   skip_on_cran()
 
   if (Sys.getenv("WF_API_KEY") == "") {
-    skip("API key not set, skipping point_link_daily test")
+    skip("API key not set, skipping point_link_monthly test")
   }
 
   api_key <- Sys.getenv("WF_API_KEY")
-
   keyring::key_set_with_value(service = "wf_api_key", password = api_key)
 
   pts <- data.frame(
     lon = c(13.4, 11.6, 9.9),
     lat = c(52.5, 51.3, 50.1),
-    date_column = c("2014-08-01", "2014-08-01", "2014-08-01")
+    date_column = c("2014-01-01", "2014-01-01", "2014-01-01")
   )
   pts_sf <- sf::st_as_sf(pts, coords = c("lon", "lat"), crs = 4326)
 
-  result <- point_link_daily(
+  result <- point_link_monthly(
     indicator = "2m_temperature",
     data = pts_sf,
     date_var = "date_column",
     time_span = 0,
     time_lag = 0,
     buffer = 0,
-    baseline = c("1970","1971"),
-    order = "ymd",
+    baseline = FALSE,
+    order = "my",
     path = tempdir(),
-    catalogue = "derived-era5-land-daily-statistics",
-    statistic = "daily_mean",
-    time_zone = "utc+00:00",
+    catalogue = "reanalysis-era5-land-monthly-means",
+    by_hour = FALSE,
     keep_raw = FALSE,
     parallel = FALSE,
     chunk_size = 50
